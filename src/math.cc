7615bfc08d657eb0ddb0ea2028affcd306a69b1e
Glorify1705 <github.9y8w2@simplelogin.com>
2023-02-26 20:45:55 -0300

    Collision detection

--- src/math.cc
+++ src/math.cc
@@ -3,6 +3,9 @@
 #include <array>
 #include <utility>
 
+#include "array.h"
+#include "transformations.h"
+
 namespace {
 
 FVec2 Normal(FVec2 v) { return FVec(-v.y, v.x).Normalized(); }
@@ -25,6 +28,8 @@ struct Edge {
 
 }  // namespace
 
+// Based on Separating Axis Theorem
+// (https://en.wikipedia.org/wiki/Hyperplane_separation_theorem).
 bool CheckOverlap(const Rectangle& a, const Rectangle& b) {
   const float a0x = a.top_left.x, a0y = a.top_left.y;
   const float a1x = a.bottom_right.x, a1y = a.top_left.y;
@@ -36,25 +41,50 @@ bool CheckOverlap(const Rectangle& a, const Rectangle& b) {
   const float b2x = b.bottom_right.x, b2y = b.bottom_right.y;
   const float b3x = b.top_left.x, b3y = b.bottom_right.y;
 
-  const std::array<FVec2, 4> kPointsA = {FVec(a0x, a0y), FVec(a1x, a1y),
-                                         FVec(a2x, a2y), FVec(a3x, a3y)};
+  const FVec2 acenter = (a.top_left + a.bottom_right) / 2;
 
-  const std::array<FVec2, 4> kPointsB = {FVec(b0x, b0y), FVec(b1x, b1y),
-                                         FVec(b2x, b2y), FVec(b3x, b3y)};
+  const FMat4x4 kRotationA = TranslationXY(acenter.x, acenter.y) *
+                             RotationZ(a.angle) *
+                             TranslationXY(-acenter.x, -acenter.y);
 
-  const std::array<FVec2, 8> kEdges = {
-      FVec(a1x - a0x, a1y - a0y), FVec(a2x - a1x, a2y - a1y),
-      FVec(a3x - a2x, a3y - a2y), FVec(a0x - a3x, a0y - a3y),
-      FVec(b1x - b0x, b1y - b0y), FVec(b2x - b1x, b2y - b1y),
-      FVec(b3x - b2x, b3y - b2y), FVec(b0x - b3x, b0y - b3y),
-  };
+  std::array<FVec2, 4> kPointsA = {FVec(a0x, a0y), FVec(a1x, a1y),
+                                   FVec(a2x, a2y), FVec(a3x, a3y)};
 
-  for (const auto& edge : kEdges) {
+  const FVec2 bcenter = (b.top_left + b.bottom_right) / 2;
+
+  const FMat4x4 kRotationB = TranslationXY(bcenter.x, bcenter.y) *
+                             RotationZ(b.angle) *
+                             TranslationXY(-bcenter.x, -bcenter.y);
+
+  std::array<FVec2, 4> kPointsB = {FVec(b0x, b0y), FVec(b1x, b1y),
+                                   FVec(b2x, b2y), FVec(b3x, b3y)};
+
+  for (auto& p : kPointsA) {
+    const FVec4 rot = kRotationA * FVec(p.x, p.y, 0, 1);
+    p.x = rot.x;
+    p.y = rot.y;
+  }
+
+  for (auto& p : kPointsB) {
+    const FVec4 rot = kRotationB * FVec(p.x, p.y, 0, 1);
+    p.x = rot.x;
+    p.y = rot.y;
+  }
+
+  FixedArray<FVec2, 8> edges;
+  for (size_t i = 0; i < kPointsA.size(); ++i) {
+    edges.Push(kPointsA[(i + 1) % kPointsA.size()] - kPointsA[i]);
+  }
+  for (size_t i = 0; i < kPointsB.size(); ++i) {
+    edges.Push(kPointsB[(i + 1) % kPointsB.size()] - kPointsB[i]);
+  }
+
+  for (const auto& edge : edges) {
     const auto normal = Normal(edge);
     const auto [a_min, a_max] = Project(kPointsA, normal);
     const auto [b_min, b_max] = Project(kPointsB, normal);
     if (a_max < b_min || b_max < a_min) return true;
   }
 
-  return true;
+  return false;
 }
