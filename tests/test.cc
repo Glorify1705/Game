0a524cb42d99c981d6baefabc90de8147afa6c44
Glorify1705 <github.9y8w2@simplelogin.com>
2023-03-29 23:16:33 +0200

    Move test setup

--- tests/test.cc
+++ tests/test.cc
@@ -1,5 +1,6 @@
 #include "array.h"
 #include "gtest/gtest.h"
+#include "lookup_table.h"
 
 namespace G {
 
@@ -33,6 +34,16 @@ TEST(Tests, FixedArrayWithAllocator) {
   EXPECT_EQ(array[0], 1);
   EXPECT_EQ(array[1], 2);
   EXPECT_EQ(array[2], 3);
+}
+
+TEST(Tests, LookupTable) {
+  LookupTable<int> table;
+  table.Insert("foo", 1);
+  table.Insert("bar", 2);
+  EXPECT_EQ(table.size(), 2);
+  int val;
+  ASSERT_TRUE(table.Lookup("foo", &val));
+  EXPECT_EQ(val, 1);
 }
 
 }  // namespace G
