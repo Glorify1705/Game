33073e1df1e90b14f19d3e3a251ab0dba467bd6d
Juan Pablo Darago <jpdarago@gmail.com>
2024-09-28 13:50:39 -0300

    Use clang instead of gcc

--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 { pkgs ? import <nixpkgs> {}}:
 
-pkgs.mkShell {
+pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
   packages = with pkgs; [
     SDL2
     SDL2_mixer
     ccls
     clang-tools
+    clang
     cmake
     fnlfmt
     gdb
