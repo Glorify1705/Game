8f823b51bb05f5b557f731e81016b599a441c34e
Juan Pablo Darago <jpdarago@gmail.com>
2024-11-08 23:37:03 -0300

    Use devenv

---
+++ devenv.nix
@@ -0,0 +1,35 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+
+  packages = with pkgs; [ 
+    SDL2
+    SDL2_mixer
+    ccls
+    clang
+    clang-tools
+    cmake
+    fnlfmt
+    gdb
+    libGL
+    python3
+    sqlite
+    sqlitebrowser
+    xorg.xrandr
+    zlib
+  ];
+  
+  env.CCLS_LOCATION = "${pkgs.ccls}/bin/ccls";
+
+  tasks."game:build" = {
+    exec = "cmake -S . -B build && cmake --build build --target Game";
+  };
+
+  tasks."game:run" = {
+    exec = "cmake -S . -B build && cmake --build build --target Run";
+  };
+
+  tasks."game:clean" = {
+    exec = "rm -rf build/*";
+  };
+}
