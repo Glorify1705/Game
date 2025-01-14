76746d83cc5d9f68ce041ef149b16d55abca57f2
Juan Pablo Darago <jpdarago@gmail.com>
2025-01-13 22:29:15 -0300

    Fix dumb double free

--- src/dictionary.h
+++ src/dictionary.h
@@ -76,7 +76,7 @@ class Dictionary {
     for (Node* child : n->child) {
       if (child != nullptr) Dealloc(child);
     }
-    allocator_->Destroy(root_);
+    allocator_->Destroy(n);
   }
 
   Allocator* allocator_;
