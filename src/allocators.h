ef07690b8b30f7f70ffe7a7c2c209761537c18e0
Juan Pablo Darago <jpdarago@gmail.com>
2025-09-20 17:03:45 -0300

    Freelist

--- src/allocators.h
+++ src/allocators.h
@@ -201,6 +201,7 @@ class FreeList {
       block->next = free_;
       free_ = block;
     }
+    DCHECK(free_ != nullptr, "Out of memory");
     T* ptr = reinterpret_cast<T*>(free_);
     free_ = free_->next;
     return ptr;
